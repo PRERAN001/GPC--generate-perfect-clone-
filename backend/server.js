c189ca448e7c8989390493b2182fcaa6325f3fe3
PRERAN S <prerans181@gmail.com>
2026-02-07 13:02:22 +0530

    Update server.js001

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app=express()
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors:{
-        origin: "https://rabbitz-nu.vercel.app/",
+        origin: "https://rabbitz-nu.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
